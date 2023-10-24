5aed0485697be112d5868530a976d6b40487202d
Maurice Mengel <mauricemengel@gmail.com>
2023-10-24 19:36:21 +0200

    rename record.py

--- src/MpApi/Record/__init__.py
+++ src/MpApi/Record/__init__.py
@@ -1,3 +1,6 @@
 """Implementation-specific logic to do things with moduleItems"""
 
 __version__ = "0.0.1"
+
+# should we import moduleItem here to make it available as MpApi.Record?
+from MpApi.Record._Record import Record
